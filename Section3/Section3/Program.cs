d8b9be5f1eb5ce01ac6f68ec99a3937a4d559d7a
Gustavo Goncalves <gustavofg7@hotmail.com>
2022-12-13 17:13:24 -0300

    add Solution Seção 3

---
+++ Section3/Section3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Section3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100; // tipo pré-definido C#
+            //SByte x = 100; // tipo .NET precisa incluir using System
+            Console.WriteLine(x);
+        }
+    }
+}
