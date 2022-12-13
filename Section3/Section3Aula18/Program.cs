465a556e2032002fcebcf8988b261f4d2d1160af
Gustavo Goncalves <gustavofg7@hotmail.com>
2022-12-13 18:43:24 -0300

    teste

--- Section3/Section3Aula18/Program.cs
+++ Section3/Section3Aula18/Program.cs
@@ -15,19 +15,21 @@ namespace Section3Aula18
             Console.WriteLine("Bom dia!");
             Console.WriteLine("Até mais.");
 
-            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
             Console.WriteLine(x);
             Console.WriteLine(x.ToString("F2"));
             Console.WriteLine(x.ToString("F4"));
             Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
 
-            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
             Console.WriteLine("RESULTADO = " + x);
             Console.WriteLine("O valor do troco é " + x + " reais");
             Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
 
-            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
             Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
+            Console.WriteLine("A paciente {0} tem {1} anos e seu sexo é: {2}", z, y, w);
+            Console.WriteLine($"A paciente {z} tem {y} anos e seu sexo é: {w}");
 
             Console.ReadLine();
         }
